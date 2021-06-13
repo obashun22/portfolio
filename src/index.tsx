1ae656572f16c4b7a19d1fe95e60fd603331e424
obashun22 <obashun22@yahoo.co.jp>
2021-06-13 18:31:14 +0900

    [add] react-router-dom・@types/react導入

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Route path="/Home" component={Home}></Route>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
